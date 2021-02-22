f5c7ca4f6e40ba92f07b52a6fed56355a9a38b67
Diego Sandoval <dsandoval@semtech.com>
2021-02-22 23:47:22 +0100

    Added script to populate db tables

---
+++ scripts/populate_db_tables.sql
@@ -0,0 +1,28 @@
+INSERT INTO trackers(deveui, label) 
+VALUES 
+(48, 'cow00'),
+(49, 'cow01'),
+(50, 'cow02'),
+(51, 'cow03'),
+(52, 'cow04'),
+(53, 'cow05'),
+(54, 'cow06'),
+(55, 'cow07'),
+(56, 'cow08'),
+(57, 'cow09'),
+(304, 'cow10');
+
+INSERT INTO cows(label, name)
+VALUES
+('Artemisa', 'cow00'),
+('Candelaria', 'cow01'),
+('Fidel', 'cow02'),
+('Goya', 'cow03'),
+('Honorio', 'cow04'),
+('Lola', 'cow05'),
+('Mechas', 'cow06'),
+('Picasso', 'cow07'),
+('Salvador', 'cow08'),
+('Tobias', 'cow09'),
+('Vikingo', 'cow10');
+
